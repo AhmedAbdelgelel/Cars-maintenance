934e7a7081e25de33170ac22739530157c035b1c
mo7ammedd <medo.mostafa22255@gmail.com>
2025-05-27 23:04:37 +0300

    add github workflow

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../services/authService');
+const { register, login, protect } = require('../services/authService');
 
 router.post('/register', register);
 router.post('/login', login);
+
+router.get('/me', protect, (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: { driver: req.driver }
+  });
+});
 
 module.exports = router;
